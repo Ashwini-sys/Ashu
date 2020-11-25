88f5fdbb680ba1a34fe3bd1889a0789ae2d5e8f1
khilesh <tichkuleashwini00@gmail.com>
2020-11-25 08:48:00 +0530

    basic

---
+++ basicRcode.R
@@ -0,0 +1,75 @@
+2+3
+6*7
+#logical value
+#if we put 4 then it gives true and if we put ==5 then it gives false
+#we have to write ==
+2+2 == 4
+2+2 == 5
+>happy learning
+"happy learning"
+X<- 42
+X
+X/7
+Y = 100
+Y 
+Y/5 
+X<- 'HAPPY LEARNING'  
+X 
+x/7 
+sum (2, 3, 5)
+sqrt(64)
+#repeat a vlaue 3 times
+rep(576, times =3 )
+rep("M Delighted" , 4)
+q<- c('a', 'b', 'c')
+q
+w<- c('reena', 'teena', 'meena')
+w
+g<- 5:9
+g
+h<- c(5:9)
+h
+i<- seq(5,9)
+i
+j <- seq(5,9,0.5)
+j
+k = seq(9,5, -0.5)
+k
+learn <- c('you', 'me', 'R')
+learn
+learn[2]
+learn[3]
+learn[1]
+learn[3] <- 'SaS'
+learn
+learn[4] <- 'SPSS'
+learn
+a <-c(1,2,3)
+a
+#add 1 to a
+a+1
+a/2
+a*2
+b<- c(4,5,6)
+b
+a+b
+a-b
+a*b
+a == c(1, 99, 3)
+m<- c(1, 3, NA, 7, 9)
+m
+sum(m, na.rm = TRUE)
+matrix(0,3,4)
+sijo<- 1:8
+sijo
+s<- matrix(sijo, c(2,4))
+s
+s[, 4]
+s[2, ]
+s[2,4]
+s[ ,2:4]
+s[1,4]
+s[, 2:3]
+s[ ,2:3]
+help(sum)
+help
